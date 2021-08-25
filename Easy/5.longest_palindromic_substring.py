4a02049dbf3b5d7891dc336900f73c82e0932905
di2mot <motsar.dm@gmail.com>
2021-08-25 21:39:42 +0300

    Update 5.longest_palindromic_substring.py

--- Easy/5.longest_palindromic_substring.py
+++ Easy/5.longest_palindromic_substring.py
@@ -23,35 +23,56 @@ Output: "a"
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) < 3:
-            return s[0]
+            if s[0] != s[-1]:
+                return s[0]
+            else: return s
 
-        stuck = []
-        n = 0
-        t_n = 0
-        t = False
+        start = 0
+        stop = 0
+        max_len = 0
+
+        def recurs_checker(s: str) -> bool:
+            if len(s) <= 1:
+                return True
+            if s[0] == s[-1]:
+                return recurs_checker(s[1:-1])
+            else: return False
 
         for x in range(len(s)):
             for y in range(len(s)-1, x, -1):
-                
                 if s[x] == s[y]:
-                    print(f'x={x}, s[x]={s[x]}, y={y}, s[y]={s[y]}')
-                    t = True
+                    print(x, y)
+                    print(s[x:y+1], len(s[x:y+1]))
+                    if len(s[x:y+1])%2 == 0:
+                        left = s[x:(len(s[x:y+1])//2)]
+                        right = s[(len(s[x:y+1])//2): y+1]
+                        print('0',left, right[::-1])
 
-                    if y - x == 2:
-                        t_n += 3
                     else:
-                        t_n += 2
+                        left = s[x:(len(s[x:y+1])//2)]
+                        right = s[(len(s[x:y+1])//2+1): y+1]
+                        print('1', left, right[::-1])
 
-                    if n < t_n: 
-                        n = t_n
-                    break
-                else:
-                    t = False
-                    t_n = 0
-        return n
+                    if left == right[::-1]:
+
+                        if len(s[x:y+1]) > max_len:
+                            max_len = len(s[x:y+1])
+                            start = x
+                            stop = y + 1
+                    # else: break
+
+                    # print(f'x={x}, s[x]={s[x]}, y={y}, s[y]={s[y]} {s[x:y+1]}')
+                    # if recurs_checker(s[x:y+1]):
+                    #     if len(n) < len(s[x:y+1]):
+                    #         n = s[x:y+1]
+                        # break
+        
+            # else: continue
+        return s[start:stop]
 
 
-a = 'baabad'
+a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+e = 'cbbd'
 s = Solution()
-b = s.longestPalindrome(a)
+b = s.longestPalindrome(e)
 print(b)
